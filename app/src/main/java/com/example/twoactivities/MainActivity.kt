2297f26f5319f37558b6f94847e4c5fa76615b53
Welishe Jude <welishejude98@gmail.com>
2022-09-20 15:54:35 +0300

    "second commit"

--- app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.twoactivities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -9,13 +10,16 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     private  val  LOG_TAG = "MainActivity"
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun launchSecondActivity(view: View) {
-       Log.d(LOG_TAG,"Button clicked!")
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent);
+
+
+        Log.d(LOG_TAG,"Button clicked!")
     }
 }
