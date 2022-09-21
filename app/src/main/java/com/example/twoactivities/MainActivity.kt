c92e6de65b500c1160949587ad5c9d4a409a1b81
kellyketahr17 <kellykethar17@gmail.com>
2022-09-21 09:50:38 +0300

    four_commit

--- app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    val EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE"
+
+    private var mMessageEditText: EditText? = null
+
     private  val  LOG_TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mMessageEditText = findViewById(R.id.editText_main)
     }
 
     fun launchSecondActivity(view: View) {
